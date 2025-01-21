4ee3e9e0d494cc08edd192f27999eb8851f32d04
arthurcunha-dev <arthurcunha1114@gmail.com>
2025-01-21 15:45:31 -0300

    feat: add configuraçao global.js

--- src/styles/Global.js
+++ src/styles/Global.js
@@ -1,16 +1,25 @@
 import { createGlobalStyle } from "styled-components";
 
-export default createGlobalStyle`
-  *{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
- }
- :root{
-    --primary: #F7F9FC;
-    --secondary: #15181C;
-    --gray:rgb(138, 137, 137);
-    --blue: #4766FF;
-    --white: #FFF;
- } 
-`
+   export default createGlobalStyle`
+   *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+   }
+      :root{
+        --primary: #F7F9FC;
+        --secondary: #15181C;
+        --gray:rgb(138, 137, 137);
+        --blue: #4766FF;
+        --white: #FFF;
+    } 
+
+    body{
+       background-color: var(--primary);
+       color: var(--secondary);
+       font-size: 1.2rem;
+        font-weight: 400;
+   } 
+   ul{list-style-type:nome;}
+   a{text-decoration: none;}
+  `
