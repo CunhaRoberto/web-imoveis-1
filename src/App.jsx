4c3a5f03edf29ea8080fa0e698334ca5dc7e17b0
arthurcunha-dev <arthurcunha1114@gmail.com>
2025-01-20 23:05:27 -0300

    feat: first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h2>Projeto web-imoveis</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles/Global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  *{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+ }
+ :root{
+    --primary: #F7F9FC;
+    --secondary: #15181C;
+    --gray:rgb(138, 137, 137);
+    --blue: #4766FF;
+    --white: #FFF;
+ } 
+`
