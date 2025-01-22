e01e41d3b247507a538c3f97a305e808bfd85f19
arthurcunha-dev <arthurcunha1114@gmail.com>
2025-01-22 17:37:56 -0300

    feat: add a logo e o menu da pagina principal

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Conteiner, Text } from "./styles";
+
+const Banner = ( ) => {
+    return (
+        <Conteiner>
+          <Text>
+            <h2>Descubra o seu próximo destino de lazer!</h2>
+            <p></p>
+            <span>Casas perfeitas para momentos inesquecíveis!!</span>
+          </Text>
+        </Conteiner>
+    )
+}
+
+export default Banner;
---
+++ src/components/Banner/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+import imagem from "../../assets/imagem/casa_2.webp"
+
+export const Conteiner = styled.div`
+    padding: 200px 150px;
+    position: realive;
+    background-size: cover;
+    background-position: center;
+     background-image: url(${imagem});
+`
+
+
+export const Text = styled.div`
+
+`
