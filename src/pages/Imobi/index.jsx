f891265642199829a5e8b1e227f6a161d013b064
arthurcunha-dev <arthurcunha1114@gmail.com>
2025-02-03 17:51:01 -0300

    feat: finalizado a parte tecnica da pagina de detalhes dos imoveis

--- src/pages/Imobi/index.jsx
+++ src/pages/Imobi/index.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { Container, Description, Left, Profile, ProfileDescription, ProfileImg, Right, Thumb } from "./styled";
+import { Container, Description, Left, Profile, ProfileContact, ProfileDescription, ProfileFormContact, ProfileImg, Right, Thumb } from "./styled";
 import TopBanner from "../../components/TopBanner";
 
 const Imobi = ( ) => {
@@ -11,10 +11,10 @@ const Imobi = ( ) => {
                 <Thumb>
                     <img src="https://www.toziimoveis.com.br/assets/blog/9-1e397c7a9726cbf88244bccb62cc5247.jpg" alt="" />
                 </Thumb>
-            </Left>
             <Description>
-                <p> .  Imóvel  pertence  a  Roberto  Aparecido  da  Cunha,  Rua  Roney  Gomes  barbosa,  número  1114</p>
+            <p>   Imóvel  pertence  a  Roberto  Aparecido  da  Cunha,  Rua  Roney  Gomes  barbosa,  número  1114</p>
             </Description>
+            </Left>
             <Right>
                 <Profile>
                   <ProfileImg>
@@ -22,10 +22,25 @@ const Imobi = ( ) => {
                   </ProfileImg>
                   <ProfileDescription>
                     <h3>Roberto Cunha</h3>
-                    <p>(11) 94799-3421</p>
-                    <p>arthurcunha1114@gmail.com</p>
+                    <p>Descrição do usuário</p>
                   </ProfileDescription>
                 </Profile>
+                <ProfileContact>
+                    <h3>Informações para contato:</h3>
+                    <p>(11) 94799-3421</p>
+                    <p>arthurcunha1114@gmail.com</p>
+                </ProfileContact>
+                <ProfileFormContact>
+                  <h3>Contate o anunciante</h3>
+                  <form>
+                    <input type="text" name="" id=""></input>
+                    <p></p>
+                    <input type="text" name="" id=""></input>
+                    
+                    <textarea name="" id="" cols="30" rows="10" />
+                    <button>Enviar mensagem</button>
+                  </form>
+                </ProfileFormContact>
             </Right>
          </Container>
         </Fragment>
